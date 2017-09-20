1d643100e6ddfd71c616ce555fa0caeda384ac60
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-19 21:09:27 -0300

    Update ApplicationController to sanitize devise parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    return if DeviseSanitizerService.perform(devise_parameter_sanitizer, resource_class)
+
+    devise_parameter_sanitizer
+  end
 end
