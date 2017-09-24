1d40c75cfd53b55d5e9ba89ade1c38a3a70c7262
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-24 14:08:43 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+
+  # namespace :authetication
+
   devise_for :users
+  devise_for :profissionals
 
   root 'events#index'
 
-  namespace :app, path: '', constraints: { format: :html } do
+  namespace :app, path: '/app', constraints: { format: :html } do
     get '/*path', to: 'users#app'
   end
 end
