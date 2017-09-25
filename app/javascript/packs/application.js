248cb1630719a6625bbdd9e61d9db5444473f145
felipe.soares <felipeluizsoares@gmail.com>
2017-09-25 14:12:31 -0300

    create a constants files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,70 +1,27 @@
-const monthsFull = [
-  'Janeiro',
-  'Fevereiro',
-  'Março',
-  'Abril',
-  'Maio',
-  'Junho',
-  'Julho',
-  'Augusto',
-  'Setembro',
-  'Outubro',
-  'Novembro',
-  'Dezembro'
-]
-
-const monthsShort = [
-  'Jan',
-  'Fev',
-  'Mar',
-  'Abr',
-  'Mai',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Set',
-  'Out',
-  'Nov',
-  'Dez'
-]
-
-const weekdaysFull = [
-  'Domingo',
-  'Segunda',
-  'Terça',
-  'Quarta',
-  'Quinta',
-  'Sexta',
-  'Sábado'
-]
-
-const weekdaysShort = [
-  'Dom',
-  'Seg',
-  'Ter',
-  'Qua',
-  'Qui',
-  'Sex',
-  'Sáb'
-]
+import {
+  monthsFull,
+  monthsShort,
+  weekdaysFull,
+  weekdaysShort
+} from './constants'
 
 $(document).ready(() => {
    $('select').material_select()
 
    $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 120, // Creates a dropdown of 15 years to control year,
+      selectMonths: true,
+      selectYears: 120,
+      closeOnSelect: false,
       labelMonthNext: 'Próximo mês',
       labelMonthPrev: 'Mês anterior',
       labelMonthSelect: 'Selecione um mês',
       labelYearSelect: 'Selecione um ano',
+      today: 'Hoje',
+      clear: 'Limpar',
+      close: 'Confirmar',
       monthsFull,
       monthsShort,
       weekdaysFull,
       weekdaysShort,
-      today: 'Hoje',
-      clear: 'Limpar',
-      close: 'Confirmar',
-      closeOnSelect: false // Close upon selecting a date,
    })
 })
---
+++ app/javascript/packs/constants.js
@@ -0,0 +1,49 @@
+export const monthsFull = [
+  'Janeiro',
+  'Fevereiro',
+  'Março',
+  'Abril',
+  'Maio',
+  'Junho',
+  'Julho',
+  'Augusto',
+  'Setembro',
+  'Outubro',
+  'Novembro',
+  'Dezembro'
+]
+
+export const monthsShort = [
+  'Jan',
+  'Fev',
+  'Mar',
+  'Abr',
+  'Mai',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Set',
+  'Out',
+  'Nov',
+  'Dez'
+]
+
+export const weekdaysFull = [
+  'Domingo',
+  'Segunda',
+  'Terça',
+  'Quarta',
+  'Quinta',
+  'Sexta',
+  'Sábado'
+]
+
+export const weekdaysShort = [
+  'Dom',
+  'Seg',
+  'Ter',
+  'Qua',
+  'Qui',
+  'Sex',
+  'Sáb'
+]
