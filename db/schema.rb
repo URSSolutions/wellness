e4b6191df37c5053acaf07c48222ea84ab85ad31
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-24 22:25:35 -0300

    Add subcription migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922013724) do
+ActiveRecord::Schema.define(version: 20170925001135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20170922013724) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_profissionals_on_email", unique: true
     t.index ["reset_password_token"], name: "index_profissionals_on_reset_password_token", unique: true
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
