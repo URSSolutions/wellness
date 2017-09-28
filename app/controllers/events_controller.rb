0a1de008115775dd1fac7a54206c4996dc33f5ac
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-28 11:04:08 -0300

    Update index to add checkout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,22 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:buy, :checkout]
+
   def index
     @events = Event.all
+  end
+
+  def checkout
+    @subscription = Subscription.new
+    @event = Event.find(params.require(:id))
+  end
+
+  def buy
+    if BuyEventService.perform(current_user, Event.find(params.require(:id)))
+      flash[:notice] = 'Compra realizada com sucesso!'
+      redirect_to '/app/'
+    else
+      flash[:error] = 'Não foi possível realizar compra.'
+      redirect_to '/'
+    end
   end
 end
