b92ee9c9b2df9d5ba7189b32f74ed911e95b07e0
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-09-28 11:03:19 -0300

    Update event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  has_many :subsciptions
-  has_many :users, through: :subsciption
+  has_many :subscription
+  has_many :users, through: :subscription
 
   validates_presence_of :name, :max_attendees, :description, :initial_date, :final_date, :price
   validates_numericality_of :max_attendees, :price, greater_than_or_equal_to: 0
