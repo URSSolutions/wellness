d7ece32f06085c86690cc7b954999b74d17453da
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-10-07 14:43:14 -0300

    Add relation event-activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,9 @@
 class Activity < ApplicationRecord
   belongs_to :user
+  belongs_to :event
 
   enum category: [ :physical_activity, :meal ]
 
-  validates_presence_of :name, :category
+  validates_presence_of :name, :category, :user, :event
   validates_presence_of :photo, if: :meal?
 end
