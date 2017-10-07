c81cac2de5c94983c83704ed944150b86857aeda
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-10-07 13:55:58 -0300

    Update serializers

--- app/serializers/professional_serializer.rb
+++ app/serializers/professional_serializer.rb
@@ -1,8 +1,5 @@
-class ProfessionalSerializer < ActiveModel::Serializer
+class ProfessionalSerializer < ApplicationSerializer
   attributes :id, :email, :first_name, :last_name, :occupation, :formation,
              :experience_years, :created_at, :updated_at, :class_name
 
-  def class_name
-    object.class.to_s
-  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,12 +1,8 @@
-class UserSerializer < ActiveModel::Serializer
+class UserSerializer < ApplicationSerializer
   attributes :id, :first_name, :last_name, :full_name, :class_name, :email,
              :created_at, :updated_at, :occupation, :phone, :birth_date,
              :gender, :height, :weight, :physical_activities, :smoke,
              :smoke_frequency, :drink, :drink_frequency, :family_diseases,
              :medicines, :daily_water_quantity, :favorite_dishes,
              :disliked_dishes, :chew
-
-  def class_name
-    object.class.to_s
-  end
 end
