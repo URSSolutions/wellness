d1906382f8b788299af0f279fe2dbf50bf94c7b1
felipe.soares <felipeluizsoares@gmail.com>
2017-10-09 19:57:18 -0300

    Update feedback reducer to be an array

--- app/javascript/packs/reducers/feedback.js
+++ app/javascript/packs/reducers/feedback.js
@@ -1,31 +1,12 @@
 import * as TYPES from '../actions/types'
 
-const DEFAULT_STATE = {
-  error: '',
-  feedbacks: [
-    {
-      profissionalName: 'Vagner',
-      date: '10/10/2017',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliq'
-    }
-  ]
-}
-
-export const feedback = (state = DEFAULT_STATE, action) => {
+export const feedback = (state = [], action) => {
   switch (action.type) {
     case TYPES.FETCH_FEEDBACKS_SUCCESS:
-      return {
-        ...state,
-        feedbacks: [
-          ...action.feedbacks
-        ]
-      }
+      return [ ...action.feedbacks ]
 
     case TYPES.FETCH_FEEDBACKS_ERROR:
-      return {
-        ...state,
-        error: action.error
-      }
+      return []
 
     default:
       return state
