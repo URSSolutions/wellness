b56880e5d1a4ca610cc3733d8a6ce3e79ac66b3a
felipe.soares <felipeluizsoares@gmail.com>
2017-10-09 12:45:49 -0300

    Add fetch auth to auth reducer

--- app/javascript/packs/reducers/auth.js
+++ app/javascript/packs/reducers/auth.js
@@ -1,15 +1,24 @@
 import * as TYPES from '../actions/types'
 
 const DEFAULT_STATE = {
-  name: 'Felipeson',
-  token: '123',
-  event: {
-    name: 'Emagrecendo em 30 dias'
-  }
+  currentAuth: {},
+  error: null
 }
 
 export const auth = (state = DEFAULT_STATE, action) => {
   switch (action.type) {
+    case TYPES.FETCH_AUTH_SUCCESS:
+      return {
+        ...state,
+        currentAuth: action.auth
+      }
+
+    case TYPES.FETCH_AUTH_ERROR:
+      return {
+        ...state,
+        error: action.error
+      }
+
     case TYPES.DISPATCH_AUTH_LOGOUT_SUCCESS:
       return {}
 
