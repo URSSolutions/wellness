31b04a2ce6812886ef38ad4ab53b84a976e43cb0
felipe.soares <felipeluizsoares@gmail.com>
2017-10-09 14:50:07 -0300

    Add activity card component

---
+++ app/javascript/packs/components/activity-card.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const handlePhoto = (activity) => {
+  const photos = {
+    meal: activity.src || 'http://www.fairfaxcounty.gov/news2/wp-content/uploads/2016/05/meal.jpg',
+    exercise: 'https://az616578.vo.msecnd.net/files/2016/03/01/635924574014208042697820024_Exercising.jpg'
+  }
+
+  return photos[activity.category]
+}
+
+const ActivityCard = ({ activity }) => {
+  return (
+    <div className='card small user-home__activity-card'>
+      <div className='card-image'>
+        <img src={ handlePhoto(activity) } />
+
+        <h3 className='card-title user-home__activity-card-title'>
+          { activity.name }
+        </h3>
+      </div>
+
+      <div className='card-content'>
+        <p> { activity.category } </p>
+
+        <p> { activity.created_at } </p>
+
+        <p> { activity.description } </p>
+      </div>
+    </div>
+  )
+}
+
+ActivityCard.propTypes = {
+  activity: PropTypes.object.isRequired
+}
+
+export default ActivityCard
