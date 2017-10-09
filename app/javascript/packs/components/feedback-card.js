ec1b4be8b7a2e7444fc4c626a310a96035451d0d
felipe.soares <felipeluizsoares@gmail.com>
2017-10-09 20:06:20 -0300

    Add a feedbackCard component

---
+++ app/javascript/packs/components/feedback-card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const FeedbackCard = ({ feedback, professional }) => {
+  return (
+    <div className='card small user-home__feedback-card'>
+      <div className='card-content'>
+        <h3> { professional.name } </h3>
+
+        <p className='user-home__feedback-date'> { feedback.created_at } </p>
+
+        <p> { feedback.description } </p>
+      </div>
+    </div>
+  )
+}
+
+FeedbackCard.propTypes = {
+  feedback: PropTypes.object.isRequired
+}
+
+export default FeedbackCard
