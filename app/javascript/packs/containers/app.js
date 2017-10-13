54b5c555cc4066a83b27c61dd0396e16ec454bb5
felipe.soares <felipeluizsoares@gmail.com>
2017-10-13 18:12:18 -0300

    Update App component to handle auth redirect logic

--- app/javascript/packs/containers/app.js
+++ app/javascript/packs/containers/app.js
@@ -3,17 +3,38 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import * as authActions from '../actions/auth'
 
+import { verifyAuthType } from '../services/verify-auth-type'
+
+import Spinner from 'react-spinkit'
 import Header from '../components/header'
+import UserHome from './user-home'
+import ProfessionalHome from './professional-home'
 
 class App extends Component {
   constructor (props) {
     super(props)
 
+    this.state = { userVerified: false, isUser: false }
+
     this.dispatchLogout = this.dispatchLogout.bind(this)
+    this.handleUserType = this.handleUserType.bind(this)
+  }
+
+  componentDidMount () {
+    verifyAuthType()
+      .then((isUser) => this.setState({ userVerified: true, isUser }))
   }
 
   dispatchLogout () {
     this.props.dispatchAuthLogout()
+  }
+
+  handleUserType () {
+    if (this.state.isUser) {
+      return <UserHome />
+    }
+
+    return <ProfessionalHome />
   }
 
   render () {
@@ -21,6 +42,17 @@ class App extends Component {
     return (
       <section>
         <Header dispatchLogout={ this.dispatchLogout } />
+
+        {
+          state.userVerified && this.handleUserType()
+        }
+
+        {
+          !state.userVerified &&
+          <div className='spinner-container'>
+            <Spinner name='folding-cube' />
+          </div>
+        }
       </section>
     )
   }
