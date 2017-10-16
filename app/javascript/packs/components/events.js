0a15f39c64780f76888235337c73572a1bc194ea
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-10-15 22:47:37 -0200

    Update events component

--- app/javascript/packs/components/events.js
+++ app/javascript/packs/components/events.js
@@ -1,26 +1,42 @@
 import React from 'react'
 
-// const User = ({ user }) => {
-//   return (
-//     <h3> user.first_name </h3>
-//   )
-// }
-
-const Event = ({ event }) => {
+const User = ({ user }) => {
   return (
-    <li>
-      <h3> { event.name } </h3>
+    <li className="collection-item">
+      <div>
+        { `${user.first_name} ${user.last_name}` }
 
-      {/* { users.map((user) => <User user={ user } />) } */}
+        <a href="#" className="secondary-content">
+          <i className="material-icons">feedback</i>
+        </a>
+      </div>
     </li>
   )
 }
 
-const Events = ({ events }) => {
+const Event = ({ event }) => {
   return (
-    <ul>
-      { events.map((event, index) => <Event key={ index } event={ event } />) }
-    </ul>
+    <li>
+      <ul className="collection with-header">
+        <li className="collection-header"><h4>{ event.name }</h4></li>
+
+        { event.users.map((user, index) => <User key={ index } user={ user } />) }
+      </ul>
+    </li>
+  )
+}
+
+const Events = ({ events, auth }) => {
+  return (
+    <div>
+      <h2 className='user-home__name'> Olá, { auth.first_name } </h2>
+
+      <h2 className='user-home__name'> Eventos: </h2>
+
+      <ul>
+        { events.map((event, index) => <Event key={ index } event={ event } />) }
+      </ul>
+    </div>
   )
 }
 
