73325769ae0420effbcb0e659dfe19fb4700615b
felipe.soares <felipeluizsoares@gmail.com>
2017-10-19 22:40:11 -0200

    Update feedback action creator

--- app/javascript/packs/actions/feedback.js
+++ app/javascript/packs/actions/feedback.js
@@ -1,9 +1,9 @@
 import * as TYPES from './types'
 import API from '../services/api'
 
-export const addFeedback = () => {
+export const addFeedback = (feedback) => {
   return (dispatch) => {
-    API.get('/api/feedbacks/')
+    API.post('/api/feedbacks/', feedback)
       .then((response) => dispatch(addFeedbackSuccess(response.data)))
       .catch((error) => dispatch(addFeedbackError(error)))
   }
