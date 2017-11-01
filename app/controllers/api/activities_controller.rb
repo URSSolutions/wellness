1303e94d9de8b3a39ce8c3b17dd383b505101d6a
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-10-31 23:38:20 -0200

    Update Api::ActivitiesController

--- app/controllers/api/activities_controller.rb
+++ app/controllers/api/activities_controller.rb
@@ -1,13 +1,15 @@
 class Api::ActivitiesController < Api::BaseController
   before_action :load_activity, except: [:index, :create]
-  before_action :authenticate_user!
+  before_action :load_day, only: [:index, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_any!, only: [:index, :show]
 
   def index
-    render json: current_user.activities
+    render json: @day.activities
   end
 
   def create
-    activity = current_user.activities.build(activity_params)
+    activity = @day.build(activity_params)
 
     if activity.save
       render json: activity
@@ -35,11 +37,17 @@ class Api::ActivitiesController < Api::BaseController
   private
 
   def load_activity
-    @activity = Activity.find(params.require(:id))
+    @activity = load_day.activities.find(params.require(:id))
   end
 
   def activity_params
     params.require(:activity).permit(:name, :description, :photo,
-                                     :category, :event_id)
+                                     :category)
+  end
+
+  def load_day
+    @day = User.find(params.require(:user_id))
+                           .subscriptions.find(params.require(:subscription_id))
+                           .days.find(params.require(:day_id))
   end
 end
