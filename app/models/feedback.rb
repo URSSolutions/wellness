40a0a74616ec6593ceb4729b642f39d4fa66da82
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-10-31 22:29:00 -0200

    Update Seeds

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,5 +1,5 @@
 class Feedback < ApplicationRecord
-  has_one :day
+  belongs_to :day
   belongs_to :professional
 
   validates_presence_of :day, :professional, :description
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,68 @@ def create_user(key)
   puts "Creating user #{key}"
   User.create!( email: "arthur#{key}@42.com", password: 'abc123',
     first_name: 'Arthur', last_name: "Dent #{key}",
-    occupation: 'Radialist', phone: '9912345678', birth_date: 25.years.ago,
-    gender: :male, height: 1.75, weight: 75.3, physical_activities: 'None',
+    occupation: 'Radialista', phone: '9912345678', birth_date: 25.years.ago,
+    gender: :male, height: 1.75, weight: 75.3, physical_activities: 'Nenhuma',
     smoke: false, smoke_frequency: nil, drink: true,
-    drink_frequency: 'Once in a week', family_diseases: 'A lot of them',
-    medicines: 'The blue one for anxiety', daily_water_quantity: '1l per day',
-    favorite_dishes: 'Pizza and pasta', disliked_dishes: 'chicken', chew: :fast)
+    drink_frequency: 'Uma vez na semana', family_diseases: 'Muitas doenças',
+    medicines: 'Um azul para ansiedade', daily_water_quantity: '1l por dia',
+    favorite_dishes: 'Pizza e Macarrão', disliked_dishes: 'Frango', chew: :fast)
 end
 
 puts 'Creating Professionals'
 first_professional = Professional.create!(email: 'marvin@42.com', password: 'abc123',
-                     first_name: 'Marvin', last_name: 'the Paranoid Robot',
-                     occupation: 'crew at Heart of Gold',
-                     formation: "Sirius Cybernetics Corporation's GPP " +
-                                '(Genuine People Personalities) technology',
+                     first_name: 'Marvin', last_name: 'O Robô Paranôico',
+                     occupation: 'Tripulante da Heart of Gold',
+                     formation: "Corporação Sirius Cybernetics GPP " +
+                                '(Personalidade Humana Genuína) Tecnologia',
                      experience_years: 10 )
 second_professional = Professional.create!(email: 'tricia@42.com', password: 'abc123',
                      first_name: 'Tricia', last_name: 'MacMillan',
-                     occupation: 'Mathematician and Astrophysicist',
-                     formation: 'Unknown', experience_years: 5 )
+                     occupation: 'Matemática and Astrofísica',
+                     formation: 'Desconhecida', experience_years: 5 )
 
 2.times do |event_number|
   puts 'Creating Event'
-  event = Event.create!(name: "Ford towel diet #{event_number}", max_attendees: 20,
-                        description: 'Using your towel(which should always be' +
-                        ' with you), to lose weight', initial_date: Time.current,
-                        final_date: 1.month.from_now, price: 99.99)
+  initial_date = (event_number + 1).month.ago
+  event = Event.create!(name: "Dieta da toalha de Ford #{event_number}", max_attendees: 20,
+                        description: 'Use sua toalha(Da qual deveria estar com' +
+                        ' você o tempo todo), para perder peso!', initial_date: initial_date,
+                        final_date: (initial_date + 1.month), price: 99.99)
 
   first_professional.events << event
   second_professional.events << event
 
-  10.times do |user_number|
+  3.times do |user_number|
     current_user = create_user(event_number.to_s + user_number.to_s)
     puts "Subscribing user #{user_number} to event #{event.id}"
-    Subscription.create(user: current_user, event: event)
+    subscription = Subscription.create!(user: current_user, event: event)
 
-    5.times do |activity_number|
-      puts "Creating meal #{activity_number} to user #{user_number}"
-      current_user.activities.create!(name: "Lunch#{activity_number.to_s}",
-                                      event: event,
-                                      photo: PHOTO, category: :meal)
+    (event.final_date.to_date - event.initial_date.to_date).to_i.times do |day_number|
+      puts "Creating day #{day_number}"
+      day = Day.create!(subscription: subscription,
+                        date: event.initial_date + day_number.day)
 
-      puts "Creating physical activity #{activity_number} to user #{user_number}"
-      current_user.activities.create!(name: "Night run - #{activity_number.to_s}",
-                                      description: '3km - 1 hour',
-                                      event: event, category: :physical)
+      3.times do |activity_number|
+        puts "Creating meal #{activity_number} to user #{user_number}"
+        day.activities.create!(name: "Almoço #{activity_number.to_s}",
+                               photo: PHOTO, category: :meal,
+                               created_at: day.date)
+      end
 
-      puts "Creating weight activity #{activity_number} to user #{user_number}"
-      current_user.activities.create!(description: (84.6 + (rand() * 10)).to_s,
-                                      event: event, category: :weight,
-                                      created_at: activity_number.days.ago)
-    end
+      puts "Creating physical activity to user #{user_number}"
+      day.activities.create!(name: "Corrida noturna",
+                             description: '3km - 1 hora',
+                             category: :physical, created_at: day.date)
 
-    puts "Creating feedback to user #{user_number}"
-    if user_number.even?
-      Feedback.create!(user: current_user, professional: first_professional,
-                       event: event, description: "Yeah, give up, we're all doomed")
-    else
-      Feedback.create!(user: current_user, professional: second_professional,
-                       event: event, description: "Nice, shoot for the stars")
+      puts "Creating weight activity to user #{user_number}"
+      day.activities.create!(description: (84.6 + (rand() * 10)).to_s,
+                             category: :weight, created_at: day.date)
+
+      puts "Creating feedback to user #{user_number}"
+      Feedback.create!(day: day, professional: first_professional,
+                       description: "Poise, desista, estamos todos perdidos")
+      Feedback.create!(day: day, professional: second_professional,
+                       description: "Boa, mire nas estrelas")
     end
   end
 end
