ea5a8198e0d2a952d1f25d87e4fa6f2fcbf709da
felipe.soares <felipeluizsoares@gmail.com>
2017-11-03 22:47:09 -0200

    Update feedback redux flow

--- app/javascript/packs/actions/feedback.js
+++ app/javascript/packs/actions/feedback.js
@@ -2,11 +2,11 @@ import * as TYPES from './types'
 import API from '../services/api'
 import { showSpinner, hideSpinner } from './spinner'
 
-export const addFeedback = (feedback) => {
+export const addFeedback = (userId, subscriptionId, dayId = 1, feedback) => {
   return (dispatch) => {
     dispatch(showSpinner())
 
-    API.post('/api/feedbacks/', feedback)
+    API.post(`api/users/${userId}/subscriptions/${subscriptionId}/days/${dayId}/feedbacks`, feedback)
       .then((response) => dispatch(addFeedbackSuccess(response.data)))
       .catch((error) => dispatch(addFeedbackError(error)))
       .then(() => dispatch(hideSpinner()))
@@ -23,6 +23,56 @@ const addFeedbackSuccess = (feedbackCreated) => {
 const addFeedbackError = (error) => {
   return {
     type: TYPES.ADD_FEEDBACK_ERROR,
+    error
+  }
+}
+
+export const updateFeedback = (userId, subscriptionId, dayId = 1, feedback) => {
+  return (dispatch) => {
+    dispatch(showSpinner())
+
+    API.put(`api/users/${userId}/subscriptions/${subscriptionId}/days/${dayId}/feedbacks`, feedback)
+      .then((response) => dispatch(updateFeedbackSuccess(response.data)))
+      .catch((error) => dispatch(updateFeedbackError(error)))
+      .then(() => dispatch(hideSpinner()))
+  }
+}
+
+const updateFeedbackSuccess = (feedbackUpdated) => {
+  return {
+    type: TYPES.UPDATE_FEEDBACK_SUCCESS,
+    feedbackUpdated
+  }
+}
+
+const updateFeedbackError = (error) => {
+  return {
+    type: TYPES.UPDATE_FEEDBACK_ERROR,
+    error
+  }
+}
+
+export const fetchFeedbacks = (userId, subscriptionId, dayId = 1) => {
+  return (dispatch) => {
+    dispatch(showSpinner())
+
+    return API.get(`api/users/${userId}/subscriptions/${subscriptionId}/days/${dayId}/feedbacks`)
+      .then((response) => dispatch(fetchFeedbacksSuccess(response.data)))
+      .catch((error) => dispatch(fetchFeedbacksError(error)))
+      .then(() => dispatch(hideSpinner()))
+  }
+}
+
+const fetchFeedbacksSuccess = (feedbacks) => {
+  return {
+    type: TYPES.FETCH_FEEDBACKS_SUCCESS,
+    feedbacks
+  }
+}
+
+const fetchFeedbacksError = (error) => {
+  return {
+    type: TYPES.FETCH_FEEDBACKS_ERROR,
     error
   }
 }
--- app/javascript/packs/reducers/feedback.js
+++ app/javascript/packs/reducers/feedback.js
@@ -2,6 +2,8 @@ import * as TYPES from '../actions/types'
 
 const DEFAULT_STATE = {
   feedbackCreated: {},
+  feedbackUpdated: {},
+  feedbacks: [],
   error: null
 }
 
@@ -16,6 +18,30 @@ export const feedback = (state = DEFAULT_STATE, action) => {
     case TYPES.ADD_FEEDBACK_ERROR:
       return {
         feedbackCreated: {},
+        error: action.error
+      }
+
+    case TYPES.UPDATE_FEEDBACK_SUCCESS:
+      return {
+        feedbackUpdated: action.feedbackUpdated,
+        error: null
+      }
+
+    case TYPES.UPDATE_FEEDBACK_ERROR:
+      return {
+        feedbackUpdated: {},
+        error: action.error
+      }
+
+    case TYPES.FETCH_FEEDBACKS_SUCCESS:
+      return {
+        feedbacks: action.feedbacks,
+        error: action.error
+      }
+
+    case TYPES.FETCH_FEEDBACKS_ERROR:
+      return {
+        feedbacks: [],
         error: action.error
       }
 
