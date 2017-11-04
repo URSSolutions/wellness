9b12ed1cdd86d8ef609daddd762d16d039173904
felipe.soares <felipeluizsoares@gmail.com>
2017-11-04 14:04:17 -0200

    Update user-home to properly show data events

--- app/javascript/packs/containers/user-home.js
+++ app/javascript/packs/containers/user-home.js
@@ -5,18 +5,26 @@ import { bindActionCreators } from 'redux'
 import * as authActions from '../actions/auth'
 import * as professionalActions from '../actions/professional'
 import * as activityActions from '../actions/activity'
+import * as feedbackActions from '../actions/feedback'
+import * as dayActions from '../actions/day'
 
 import Header from './header'
 import ActivityModal from '../components/activity-modal'
 import LastFeedback from '../components/last-feedback'
 import Activities from '../components/activities'
 
+import { formatSimpleDate } from '../services/format-date'
+
+
 class UserHome extends Component {
   constructor (props) {
     super(props)
 
+    this.state = { event: undefined }
+
     this.handleFetchProfessional = this.handleFetchProfessional.bind(this)
     this.handleAddActivity = this.handleAddActivity.bind(this)
+    this.handleFetchCurrentDay = this.handleFetchCurrentDay.bind(this)
   }
 
   componentDidMount () {
@@ -39,6 +47,19 @@ class UserHome extends Component {
       .then(() => fetchAuth())
   }
 
+  handleEvent (eventId) {
+    const event = this.props.events.find((event) => event.id === eventId)
+    const subscription = this.props.auth.subscriptions.find((subscription) => subscription.event_id === eventId)
+
+    this.setState({ event, subscription })
+
+    this.handleFetchCurrentDay(subscription)
+  }
+
+  handleFetchCurrentDay (subscription) {
+    this.props.fetchCurrentDay(this.props.auth.id, subscription.id)
+  }
+
   render () {
     const { state, props } = this
 
@@ -53,16 +74,35 @@ class UserHome extends Component {
             <div>
               <h2 className='user-home__name'> Olá, { props.auth.first_name } </h2>
 
-              <h2 className='user-home__name'> Evento: </h2>
+              <h2 className='user-home__name'> Eventos: </h2>
+
+              <p>Selecione o evento desejado: </p>
 
               {
                 !!props.events.length &&
-                <h2 className='user-home__event-name' > { props.events[0].name } </h2>
+                props.events.map((event, index) =>
+                  <button key={ index }
+                    className='waves-effect waves-light btn blue lighten-1'
+                    onClick={ () => this.handleEvent(event.id) }
+                  >
+                    { event.name }
+                  </button>
+                )
               }
 
               {
                 !props.events.length &&
                 <h2> Você não comprou nenhum evento ainda! </h2>
+              }
+
+              {
+                state.event &&
+                <div>
+                  <h2> Evento: { state.event.name } </h2>
+                  <p> Descrição: { state.event.description } </p>
+                  <p> Data de início: { formatSimpleDate(state.event.inital_date) } </p>
+                  <p> Data de término: { formatSimpleDate(state.event.final_date) } </p>
+                </div>
               }
             </div>
 
@@ -96,6 +136,7 @@ UserHome.propTypes = {
   activities: PropTypes.array.isRequired,
   fetchAuth: PropTypes.func.isRequired,
   fetchProfessional: PropTypes.func.isRequired,
+  fetchCurrentDay: PropTypes.func.isRequired,
   addActivity: PropTypes.func.isRequired
 }
 
@@ -103,8 +144,8 @@ const mapStateToProps = (state) => {
   return {
     auth: state.auth.currentUser,
     events: state.auth.currentUser.events,
-    feedbacks: state.auth.currentUser.feedbacks,
-    activities: state.auth.currentUser.activities,
+    feedbacks: state.day.currentDay.feedbacks,
+    activities: state.day.currentDay.activities,
     professional: state.professional.currentProfessional
   }
 }
@@ -113,7 +154,9 @@ const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     ...authActions,
     ...professionalActions,
-    ...activityActions
+    ...activityActions,
+    ...feedbackActions,
+    ...dayActions
   }, dispatch)
 }
 
