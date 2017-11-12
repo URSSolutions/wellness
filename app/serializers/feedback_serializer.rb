80b22a407ef8f98fbfb155bb88f3ffb541c409df
felipe.soares <felipeluizsoares@gmail.com>
2017-11-12 15:33:26 -0200

    Add professional_id to feedback serializer

--- app/serializers/feedback_serializer.rb
+++ app/serializers/feedback_serializer.rb
@@ -1,6 +1,6 @@
 class FeedbackSerializer < ApplicationSerializer
-  attributes :id, :description, :updated_at, :created_at
-  
+  attributes :id, :description, :updated_at, :created_at, :professional_id
+
   belongs_to :day
   belongs_to :professional
 end
